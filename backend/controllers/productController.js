b66c1d78c2e68df6a6989368c7de0194b773296a
Aparna <aparnabhattacharya2000@gmailcom>
2023-06-07 23:49:08 +0530

    I changed some codes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,25 +2,20 @@ const Product = require('../models/product');
 
 exports.create_product = async (req, res) => {
  try{
+
     const {title, description,price, imgUrl } = req.body;
 
-    if (price < 1) {
-      res.json({msg: "price must be greater then 0"});
-    } else{
-   
     let newProduct = new Product({
      title,
      description,
      price,
      imgUrl, 
       });
-  
      newProduct = await newProduct.save();
 
      res.status(201).json(newProduct)
    }
-  
- } catch(e){
+   catch(e){
     res.status(500).json({ error: e.message });
  }
 }; 
@@ -39,13 +34,32 @@ exports.products = async (req, res) => {
 exports.product = async (req,res) => {
    try{
       const id = req.params.id;
-      console.log(id);
+
       const product = await Product.findById(id);
       res.json(product)
    } catch (e) {
       res.status(500).json({error: e.message})
    }
 };
+
+exports.update_product = async (req, res) => {
+   try{
+
+      const {title, description,price, imgUrl } = req.body;
+
+      let updatedProduct = new Product({
+       title,
+       description,
+       price,
+       imgUrl, 
+       _id: req.params.id,
+        });
+
+      await Product.findByIdAndUpdate(req.params.id, updatedProduct)
+        }catch(e){
+         res.status(500).json({error: e.message})
+   }
+}
 
 
 
